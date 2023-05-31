24bc492b20e1e9d499d693e2d6dd6f28015f6897
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-05-31 18:17:46 +0530

    Added sample.

---
+++ CustomizeShimmer_CustomView/CustomizeShimmer_CustomView/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomizeShimmer_CustomView;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CustomizeShimmer_CustomView/CustomizeShimmer_CustomView/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomizeShimmer_CustomView;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
